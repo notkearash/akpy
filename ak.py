3c7f7a79f83d172fc8fa7087a8de7b887be38ade
Kearash <kearash@pm.me>
2023-02-10 13:58:57 -0500

    fix: better FileExistsError handling

--- ak.py
+++ ak.py
@@ -31,32 +31,35 @@ def arg_parse():
 def main():
     opt = 'w' if arg_parse().overwrite else 'x'
     with keyboard.Events() as events:
-        with open(arg_parse().output, opt) as file:
-            for event in events:
-                if str(event).startswith('Release'):
-                    pass
-                elif event.key == keyboard.Key.space:
-                    file.write(' ')
-                elif event.key == keyboard.Key.enter:
-                    file.write('\n')
-                elif str(event.key).startswith('Key'):
-                    file.write(
-                        "[" + str(event.key).split(".")[1].upper() + "]")
-                elif str(event.key).startswith('<'):
-                    signal_number = int(
-                        str(event.key).split('<')[1].split('>')[0])
-                    numpad_number = signal_number - 96
-                    key = '[NumPad' + str(numpad_number) + \
-                        ']' if numpad_number != 14 else '.'
-                    content = str(numpad_number) if arg_parse().quiet else key
-                    file.write(content)
-                else:
-                    file.write(str(event.key).split("'")[1])
+        try:
+            with open(arg_parse().output, opt) as file:
+                for event in events:
+                    if str(event).startswith('Release'):
+                        pass
+                    elif event.key == keyboard.Key.space:
+                        file.write(' ')
+                    elif event.key == keyboard.Key.enter:
+                        file.write('\n')
+                    elif str(event.key).startswith('Key'):
+                        file.write(
+                            "[" + str(event.key).split(".")[1].upper() + "]")
+                    elif str(event.key).startswith('<'):
+                        signal_number = int(
+                            str(event.key).split('<')[1].split('>')[0])
+                        numpad_number = signal_number - 96
+                        key = '[NumPad' + str(numpad_number) + \
+                            ']' if numpad_number != 14 else '.'
+                        content = str(
+                            numpad_number) if arg_parse().quiet else key
+                        file.write(content)
+                    else:
+                        file.write(str(event.key).split("'")[1])
+        except FileExistsError:
+            print('\033[91m\033[1mFile exists! specify -w to overwrite.\033[0m')
 
 
 if __name__ == '__main__':
     try:
-        print('\033[90m\033[1mRunning...\033[0m')
         if arg_parse().kill:
             process = multiprocessing.Process(target=main, name="Main")
             process.start()
@@ -65,7 +68,6 @@ if __name__ == '__main__':
             process.join()
         else:
             main()
+        print('\033[90m\033[1mRunning...\033[0m')
     except KeyboardInterrupt:
         print('\033[91m\033[1mProgram Terminated\033[0m')
-    except FileExistsError:
-        print('\033[91m\033[1mFile exists! specify -w to overwrite.\033[0m')
