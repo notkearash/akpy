be73ad797ab5f5c9d3704a4ec499b24c569710e6
Kearash <kearash@pm.me>
2023-02-10 13:54:07 -0500

    feat: timed process kill option added

--- ak.py
+++ ak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import multiprocessing
 import argparse
+import time
 
 from pynput import keyboard
 
@@ -18,6 +20,8 @@ def arg_parse():
                         help="overwrites the file, if it does exist", action="store_true")
     parser.add_argument('-q', '--quiet',
                         help="less verbose output", action="store_true")
+    parser.add_argument('-k', '--kill',
+                        help="kills the program after specified time (seconds)", metavar="S")
     parser.add_argument('--version', help="shows the version number",
                         action="version", version='%(prog)s v{version}'.format(version=__version__))
     args = parser.parse_args()
@@ -39,9 +43,11 @@ def main():
                     file.write(
                         "[" + str(event.key).split(".")[1].upper() + "]")
                 elif str(event.key).startswith('<'):
-                    signal_number = int(str(event.key).split('<')[1].split('>')[0])
+                    signal_number = int(
+                        str(event.key).split('<')[1].split('>')[0])
                     numpad_number = signal_number - 96
-                    key = '[NumPad' + str(numpad_number) + ']' if numpad_number != 14 else '.'
+                    key = '[NumPad' + str(numpad_number) + \
+                        ']' if numpad_number != 14 else '.'
                     content = str(numpad_number) if arg_parse().quiet else key
                     file.write(content)
                 else:
@@ -51,7 +57,14 @@ def main():
 if __name__ == '__main__':
     try:
         print('\033[90m\033[1mRunning...\033[0m')
-        main()
+        if arg_parse().kill:
+            process = multiprocessing.Process(target=main, name="Main")
+            process.start()
+            time.sleep(int(arg_parse().kill))
+            process.terminate()
+            process.join()
+        else:
+            main()
     except KeyboardInterrupt:
         print('\033[91m\033[1mProgram Terminated\033[0m')
     except FileExistsError:
